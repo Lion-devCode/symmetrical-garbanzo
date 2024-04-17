8f5bd5b5d417c565b0f2ed031d457b2eced41903
Lion Code <liondevcode@gmail.com>
2024-04-17 03:51:39 -0400

    Nuevas funciones para el codigo de la ramita feliz, ahora se sabe que es una rama de un manzano

--- ramitaCode.py
+++ ramitaCode.py
@@ -2,3 +2,8 @@ print("Hola soy una ramita que cuenta hasta 10!");
 
 for i in range(0,10):
     print(i+1);
+
+
+# Justo ahora estoy a medio codigo. Funciona pero no tan bien...
+
+print("Esta ramita tiene una flor de manzana muy bonita que va a seguir creciendo en el futuro :D");
