126f5f2e08603f7fc70fa66869651b96d0837e65
Lion Code <liondevcode@gmail.com>
2024-04-17 03:22:12 -0400

    Se vienen conflictos

--- helloGit2.py
+++ helloGit2.py
@@ -3,3 +3,7 @@ En este archivo voy ahora a saludar de distinas maneras a git
 Hola git 
 
 print("Hola git, te saludo desde mi version 2 de hello git 2");
+
+Ahora me da la loquera y le pongo mas texto al final de este archivo
+
+AAAAAAA
