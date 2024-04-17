327b52ca3b306ad92d1741094e0d5fa78580fbe4
Lion Code <liondevcode@gmail.com>
2024-04-17 04:07:08 -0400

    Hemos terminado de trabajar con esta rama

--- ramitaCode.py
+++ ramitaCode.py
@@ -7,3 +7,8 @@ for i in range(0,10):
 # Justo ahora estoy a medio codigo. Funciona pero no tan bien...
 
 print("Esta ramita tiene una flor de manzana muy bonita que va a seguir creciendo en el futuro :D");
+
+
+print("Somos un arbol de manzana!!!");
+
+# Finalmente hemos terminado de programar aqui. Estamos listos para regresar a produccion.
